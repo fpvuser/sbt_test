b93175eadefafcf245fe952679d80facdd012f73
Pavel <saswwwx@gmail.com>
2018-08-31 20:24:10 +0300

    initial commit

---
+++ src/main/scala/com/sbt_pack/Manners.scala
@@ -0,0 +1,13 @@
+package com.sbt_pack
+
+import org.apache.spark.sql.DataFrame
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.Dataset
+
+object Manners{
+
+  def happyData()(df: DataFrame): DataFrame = {
+    df.withColumn("happy", lit("data is fun"))
+  }
+
+}
